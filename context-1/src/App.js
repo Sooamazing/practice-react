fb5ec926c471b29448881bb1d1e17a1a561f2c71
Sooamazing <honang515@gmail.com>
2024-10-01 17:33:11 +0900

    feat: mono repo로 구성, context 연습 directory 추가

---
+++ context-1/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Resume from "./pages/resume";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Resume />
+      </header>
+    </div>
+  );
+}
+
+export default App;
