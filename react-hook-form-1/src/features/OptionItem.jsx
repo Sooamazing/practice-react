d360937abd01c6166b8843b2de07093d0ad2724e
Sooamazing <honang515@gmail.com>
2024-10-27 22:35:58 +0900

    fix: [react-hook-form] 1개만 있는 경우, 삭제 버튼 보이지 않도록 수정 - 기존: 1개만 있을 때 삭제 누르면 2개가 보였음.(왜?)

--- react-hook-form-1/src/features/OptionItem.jsx
+++ react-hook-form-1/src/features/OptionItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from "reactstrap";
+import {Button} from "reactstrap";
 import OptionNameField from "./OptionNameField";
 import OptionValueField from "./OptionValueField";
 import OptionPriceField from "./OptionPriceField";
@@ -8,13 +8,13 @@ import OptionPriceField from "./OptionPriceField";
  * field 옵션 추가 및 삭제
  * 입력 시 렌더링 같이 되는 문제 - 각 필드에 대한 컴포넌트로 분리(useController 분리)
  */
-function OptionItem({key, index, onClickRemoveOption }) {
+function OptionItem({index, onClickRemoveOption, showDeleteButton}) {
     return (
-        <div key={key} style={{ display: "flex", flexDirection: "column", gap: "2px" }}>
-            <Button onClick={onClickRemoveOption}>삭제</Button>
-            <OptionNameField  index={index} />
-            <OptionValueField index={index} />
-            <OptionPriceField index={index} />
+        <div style={{display: "flex", flexDirection: "column", gap: "2px"}}>
+            {showDeleteButton && <Button onClick={onClickRemoveOption}>삭제</Button>}
+            <OptionNameField index={index}/>
+            <OptionValueField index={index}/>
+            <OptionPriceField index={index}/>
         </div>
     );
 }
--- react-hook-form-1/src/layouts/ThreeStep.jsx
+++ react-hook-form-1/src/layouts/ThreeStep.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Button } from "reactstrap";
-import { useFormContext, useFieldArray } from "react-hook-form";
+import {Button} from "reactstrap";
+import {useFormContext, useFieldArray} from "react-hook-form";
 import OptionItem from "../features/OptionItem";
 
 /**
  * useFieldArray 이용 사용 옵션값 추가 및 삭제
- TODO: 맨 처음에 옵션 필드 1개 추가된 상태로 렌더링 필요.
+ * 맨 처음에 옵션 필드 1개 추가된 상태로 렌더링 -> UseEffect / length, button show 중 후자 택.
  * 참고: https://techblog.pet-friends.co.kr/react-hook-form-으로-폼-form-화면을-개선해요-e428b78539c0
  */
 function ThreeStep() {
-    const { control } = useFormContext();
-    const { fields: productOptions, append, remove } = useFieldArray({
+    const {control} = useFormContext();
+    const {fields: productOptions, append, remove} = useFieldArray({
         name: 'options',
         control,
-        defaultValues: [{ optionName: '', optionValue: '', optionPrice: '' }]
+        defaultValues: [{optionName: '', optionValue: '', optionPrice: ''}]
     });
 
     // 처음 렌더링 시 기본 옵션 필드 추가
@@ -39,12 +39,14 @@ function ThreeStep() {
     };
 
     return (
-        <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+        <div style={{display: "flex", flexDirection: "column", gap: "10px"}}>
             {productOptions.map((option, index) => (
                 <OptionItem
                     key={option.id}
                     index={index}
                     onClickRemoveOption={() => onClickRemoveOption(index)}
+                    showDeleteButton={productOptions.length > 1}
+
                 />
             ))}
             <Button onClick={onClickAddOption}>옵션값 추가</Button>
