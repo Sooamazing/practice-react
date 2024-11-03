553ba7c0ce3066479cb8e667c0e6d2b0946491f8
Sooamazing <honang515@gmail.com>
2024-11-03 21:31:44 +0900

    fix: [react-hook-form] 맨 처음 Undefined -> type error, value 없는 경우 ''로 세팅

--- react-hook-form-1/src/layouts/TwoStep.jsx
+++ react-hook-form-1/src/layouts/TwoStep.jsx
@@ -21,6 +21,7 @@ function TwoStep() {
     }
 
     const formatPhoneNumber = (value) => {
+        if(!value) return ''; // Uncaught TypeError: Cannot read properties of undefined (reading 'replace')
         const numbers = value.replace(/\D/g, '');
         if (numbers.length <= 3) return numbers;
         if (numbers.length <= 7) return `${numbers.slice(0, 3)}-${numbers.slice(3)}`;
