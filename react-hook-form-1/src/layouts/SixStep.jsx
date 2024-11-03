3e9c7188976f8f92a57f0efa9c5b3937d602ffc8
Sooamazing <honang515@gmail.com>
2024-11-03 22:58:14 +0900

    fix: [react-hook-form] 무한 더링 해결

--- react-hook-form-1/src/layouts/SixStep.jsx
+++ react-hook-form-1/src/layouts/SixStep.jsx
@@ -1,94 +1,62 @@
 import React from 'react';
 import {useController, useFormContext, useWatch} from "react-hook-form";
 
-/**
- * 특정 필드에 입력이 있을 때 다른 필드가 required 속성을 가짐
- * @returns {Element}
- * @constructor
- */
 function SixStep() {
-    const {control, setValue, clearErrors} = useFormContext();
+    const {control, clearErrors, trigger} = useFormContext();
     const {field: hobby, fieldState: {error: hobbyError}} = useController({name: 'a.b.hobby', control});
     const hobbyValue = useWatch({control, name: 'a.b.hobby'});
 
-    // hobby에 영향 받음
-    const favoriteHobbyRules = {
-        required: {
-            value: !!hobbyValue, message: 'Favorite Hobby is required'
-        }
+    // 조건부 required 규칙
+    const commonRequiredRule = { required: hobbyValue ? 'This field is required' : false };
 
-    }
     const {field: favoriteHobby, fieldState: {error: favoriteHobbyError}} = useController({
-        name: 'a.b.favoriteHobby',
-        control,
-        rules: {...favoriteHobbyRules},
+        name: 'a.b.favoriteHobby', control, rules: commonRequiredRule,
     });
-
-    const startTimeRules =
-        {
-            required: {
-                value: !!hobbyValue, message: 'Start Time is required'
-            }
-        }
-
     const {field: startTime, fieldState: {error: startTimeError}} = useController({
-        name: 'a.b.startTime',
-        control,
-        rules: {
-            ...startTimeRules
-        }
+        name: 'a.b.startTime', control, rules: commonRequiredRule,
     });
-
-    const endTimeRules = {
-        required: {
-            value: !!hobbyValue, message: 'End Time is required'
-        }
-    }
     const {field: endTime, fieldState: {error: endTimeError}} = useController({
-        name: 'a.b.endTime',
-        control,
-        rules: {
-            ...endTimeRules
-        }
+        name: 'a.b.endTime', control, rules: commonRequiredRule,
     });
 
-    // Hobby에 영향 받지 않음.
     const {field: color, fieldState: {error: colorError}} = useController({name: 'a.b.color', control});
 
-
-    // hobby 값이 사라지면 다른 필드 필수 에러 제거
-    // TODO required 속성만 제거 필요
-    if (!hobbyValue) {
-        clearErrors(['a.b.favoriteHobby', 'a.b.startTime', 'a.b.endTime']);
-    }
-
-    // 다른 필드에 입력이 생기면 에러 제거
-    // TODO 입력되지 않는 문제 해결 필요
     const handleInputChange = (name) => {
         clearErrors(name);
+    };
+
+    const handleHobbyChange = (e) => {
+        hobby.onChange(e);
+
+        // hobby 값에 따라 필수 상태 트리거
+        // Uncaught Error: Too many re-renders. React limits the number of renders to prevent an infinite loop. -> onChange에만 검증
+        // TODO 좀 더 상세히 알아 보기
+        if (e.target.value) {
+            trigger(['a.b.favoriteHobby', 'a.b.startTime', 'a.b.endTime']);
+        } else {
+            clearErrors(['a.b.favoriteHobby', 'a.b.startTime', 'a.b.endTime']);
+        }
     };
 
     return (
         <div>
             <h1>Six step</h1>
-            <input {...hobby} placeholder="Hobby"/>
+            <input {...hobby} placeholder="Hobby" onChange={handleHobbyChange}/>
             {hobbyError && <span>hobbyError: {hobbyError.message}</span>}
             <br/>
 
-            {favoriteHobbyRules?.required && <span>favoriteHobbyRules: "required"</span>}
-            <input {...favoriteHobby} placeholder="Favorite Hobby"
-                   onChange={() => handleInputChange('a.b.favoriteHobby')}/>
+            <input {...favoriteHobby} placeholder="Favorite Hobby" onChange={() => handleInputChange('a.b.favoriteHobby')}/>
             {favoriteHobbyError && <span>favoriteHobby: {favoriteHobbyError.message}</span>}
             <br/>
-            {startTimeRules?.required && <span>startTimeRules: "required"</span>}
+
             <input {...startTime} placeholder="Start Time" onChange={() => handleInputChange('a.b.startTime')}/>
             {startTimeError && <span>startTime: {startTimeError.message}</span>}
             <br/>
 
-            {endTimeRules?.required && <span>endTimeRules: "required"</span>}
             <input {...endTime} placeholder="End Time" onChange={() => handleInputChange('a.b.endTime')}/>
             {endTimeError && <span>endTime: {endTimeError.message}</span>}
             <br/>
+
             <input {...color} placeholder="Color"/>
             {colorError && <span>color: {colorError.message}</span>}
         </div>
